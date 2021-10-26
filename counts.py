ad8e651304ef09f6c83b2bfeceab471759a542b2
Sourya Vatsyayan <sourya@deepsource.io>
2021-10-26 22:25:51 +0530

    fix syntax errors

--- counts.py
+++ counts.py
@@ -1,4 +1,4 @@
 from .models import Fisherman, Retailer
 
 total_fishermen = Fisherman.objects.all().len()
-refistered_retailers = Retailer.objects.get().filter("licensed"=True).len()
+refistered_retailers = Retailer.objects.get().filter(licensed=True).len()
--- recent.py
+++ recent.py
@@ -1,4 +1,4 @@
 from .models import Fisherman, Retailer
 
 last_fisherman = Fisherman.objects.all().order_by('created')[0]
-last_registered_retailer = Retailer.objects.all().filter('licensed'=True).order_by('created')[0]
+last_registered_retailer = Retailer.objects.all().filter(licensed=True).order_by('created')[0]
